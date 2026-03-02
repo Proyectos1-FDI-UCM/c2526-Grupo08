a08ea9e8c4a2759ae1cafaed9efe304b0b6bc348
Celia-lab <cegarc17@ucm.es>
2026-03-02 18:43:40 +0100

    Colisión bala-pared arreglada

--- Assets/Scripts/InGame_Scripts/Bullet.cs
+++ Assets/Scripts/InGame_Scripts/Bullet.cs
@@ -101,8 +101,11 @@ public class Bullet : MonoBehaviour
     /// 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        _health = other.GetComponent<Health>();   
-        if(other != null) _health.Damage(_damage);
+        _health = other.GetComponent<Health>();
+        if (_health != null)
+        {
+            _health.Damage(_damage);
+        }
         Destroy(gameObject);
     }
 
