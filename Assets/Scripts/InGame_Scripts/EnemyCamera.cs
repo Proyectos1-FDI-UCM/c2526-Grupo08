d74f43bcff70a4d8dbfef5190fcbdd6fb30297d5
adrife15-oss <237634851+adrife15-oss@users.noreply.github.com>
2026-03-17 22:34:38 +0100

    Enemigo Cámara
    Enemigo inmóvil acabado funciona similar a el enemigo normal

---
+++ Assets/Scripts/InGame_Scripts/EnemyCamera.cs
@@ -0,0 +1,124 @@
+//---------------------------------------------------------
+// Breve descripción del contenido del archivo
+// Adriana Fernández Luna
+// No Way Down
+// Proyectos 1 - Curso 2025-26
+//---------------------------------------------------------
+
+using UnityEngine;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
+/// </summary>
+public class EnemyCamera : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // públicos y de inspector se nombren en formato PascalCase
+    // (palabras con primera letra mayúscula, incluida la primera letra)
+    // Ejemplo: MaxHealthPoints
+
+    [Header("Waypoints")]
+    [SerializeField] private Transform[] WayPoints;
+
+    [SerializeField] private float PointReachedDistance = 0.1f;
+
+    [Header("Movimiento")]
+    [SerializeField] private float Speed = 2f;
+
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // privados se nombren en formato _camelCase (comienza con _, 
+    // primera palabra en minúsculas y el resto con la 
+    // primera letra en mayúsculas)
+    // Ejemplo: _maxHealthPoints
+
+    private Rigidbody2D _rb;
+    private int _currentPoint = 0;
+
+    #endregion
+
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        Patrol();
+    }
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+
+    #endregion
+
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+
+    private void Patrol()
+    {
+        if (WayPoints == null || WayPoints.Length == 0) return;
+
+        Vector2 target = WayPoints[_currentPoint].position;
+
+        Vector2 direction = (target - (Vector2)transform.position).normalized;
+        _rb.linearVelocity = direction * Speed;
+
+        if (Vector2.Distance(transform.position, target) <= PointReachedDistance)
+        {
+            _currentPoint = (_currentPoint + 1) % WayPoints.Length;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Health playerHealth = other.GetComponent<Health>();
+
+            if (playerHealth != null)
+            {
+                int vidaActual = playerHealth.GetCurrentHealth();
+                playerHealth.Damage(vidaActual);
+            }
+        }
+    }
+    #endregion
+}
+
+// class EnemyCamera 
+// Adriana Fernández Luna
